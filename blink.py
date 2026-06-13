078927b08ee6b213c174337b7bc0da30fbfa3beb
Sabina Lazri <sabina_lazri1@sluz.ch>
2026-06-13 20:13:06 +0200

    add more functions for the lights

--- blink.py
+++ blink.py
@@ -1,68 +1,65 @@
 from machine import Pin
-from utime import sleep
-import time
+from utime import sleep_ms, ticks_ms, ticks_diff
 
-from machine import Pin
-import time
+car_green = Pin(8, Pin.OUT)
+car_yellow = Pin(9, Pin.OUT)
+car_red = Pin(10, Pin.OUT)
 
-carGreen = Pin(8, Pin.OUT)
-carYellow = Pin(9, Pin.OUT)
-carRed = Pin(10, Pin.OUT)
-
-pedGreen = Pin(16, Pin.OUT)
-pedYellow = Pin(17, Pin.OUT)
-pedRed = Pin(18, Pin.OUT)
-
+ped_green = Pin(16, Pin.OUT)
+ped_yellow = Pin(17, Pin.OUT)
+ped_red = Pin(18, Pin.OUT)
 
 def set_car(red, yellow, green):
-    carRed.value(red)
-    carYellow.value(yellow)
-    carGreen.value(green)
-
+    car_red.value(red)
+    car_yellow.value(yellow)
+    car_green.value(green)
 
 def set_ped(red, yellow, green):
-    pedRed.value(red)
-    pedYellow.value(yellow)
-    pedGreen.value(green)
-    
+    ped_red.value(red)
+    ped_yellow.value(yellow)
+    ped_green.value(green)
+
 def cars_green():
     set_car(0, 0, 1)
     set_ped(1, 0, 0)
+    
+def car_to_red():
+    set_car(0, 1, 0)
+    sleep_ms(2000)
+    set_car(1, 0, 0)
+    
+def car_to_red_yellow():
+    set_car(1, 1, 0)
+    sleep_ms(2000)
+    set_car(0, 0, 1)
+
+def pedestrian_green():
+    set_ped(0, 0, 1)
+    set_car(1, 0, 0)
+
+def pedestrian_to_red():
+    set_ped(0, 1, 0)
+    sleep_ms(1000)
+    set_ped(1, 0, 0)
 
 def switch_to_pedestrians():
+    set_car(0, 1, 0)
+    sleep(2)
+    set_car(1, 0, 0)
+    sleep(1)
 
-    # Grün, Orange
-    set_car(0,1,0)
-    time.sleep(2)
+    set_car(1, 1, 0)
+    sleep(2)
+    set_car(0, 0, 1)
 
-    # Rot
-    set_car(1,0,0)
-    time.sleep(1)
+    set_ped(0, 0, 1)
+    sleep(5)
 
-    # Grün
-    set_ped(0,0,1)
+    set_ped(0, 1, 0)
+    sleep(1)
+    set_ped(1, 0, 0)
 
-    time.sleep(5)
-    
-    set_ped(0,1,0)
-    time.sleep(1)
-
-    # Rot
-    set_ped(1,0,0)
-
-    # Rot + Orange
-    set_car(1,1,0)
-    time.sleep(2)
-
-    # Grün
-    set_car(0,0,1)
-    
-waiting_cars = False
-waiting_pedestrians = True
-
-if waiting_pedestrians:
-    switch_to_pedestrians()
-
-carRed.off()
-pedRed.off()
-print("Finished.")
+    set_car(1, 1, 0)
+    sleep(2)
+    set_car(0, 0, 1)
+    set_ped(1, 0, 0)
