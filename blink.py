b71294168bbea1112633cb1dd9297ee9e5600777
Sabina Lazri <sabina_lazri1@sluz.ch>
2026-06-13 16:03:16 +0200

    first steps

--- blink.py
+++ blink.py
@@ -1,19 +1,42 @@
 from machine import Pin
 from utime import sleep
+import time
+
 onBoard = Pin("LED", Pin.OUT)
-led1 = Pin(8, Pin.OUT)
-led2 = Pin(18, Pin.OUT)
+carGreen = Pin(8, Pin.OUT)
+carRed = Pin(10, Pin.OUT)
+carYellow = Pin(9, Pin.OUT)
+
+pedestrianRed = Pin(18, Pin.OUT)
+pedestrianYellow = Pin(17, Pin.OUT)
+pedestrianGreen = Pin(16, Pin.OUT)
+
 while True:
     try:
-        onBoard.toggle()
-        led1.toggle()
-        led2.toggle()
-        sleep(1) # sleep 1sec
+        carGreen.value(1)
+        carYellow.value(0)
+        carRed.value(0)
+        
+        pedestrianGreen.value(0)
+        pedestrianYellow.value(0)
+        pedestrianRed.value(1)
+        
+        time.sleep(3)
+        
+        carGreen.value(0)
+        carYellow.value(0)
+        carRed.value(1)
+        
+        pedestrianGreen.value(1)
+        pedestrianYellow.value(0)
+        pedestrianRed.value(0)
+        
+        
     except KeyboardInterrupt:
         break
     sleep(1) # sleep 1sec
 
 onBoard.off()
-led1.off()
-led2.off()
+carRed.off()
+pedestrianRed.off()
 print("Finished.")
